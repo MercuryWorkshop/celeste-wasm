2c2e1c3b4a41877f79db6fcda12f2f8913e05264
BomberFish <bomberfish@bomberfish.ca>
2024-05-23 12:33:16 -0400

    nicer splash screen

--- src/main.js
+++ src/main.js
@@ -38,8 +38,32 @@ function IntroSplash() {
         padding: 2em;
 
         pre {
-            display: inline;
+            display: inline!important;
         }
+
+        animation: fadein 0.7s ease 0s 1;
+    }
+
+    button,
+    #bar {
+      animation: fadein 0.15s ease 0s 1;
+    }
+
+    #progress {
+      transition: width 0.2s ease;
+    }
+
+    .inner {
+      margin-inline: 0.8rem;
+    }
+
+    pre {
+      font-size: 1rem;
+    }
+
+    @keyframes fadein {
+        from { opacity: 0; transform: translateY(1em); }
+        to { opacity: 1; transform: translateY(0); }
     }
     `
 
@@ -142,16 +166,29 @@ function IntroSplash() {
         <main class=${[use(store.theme)]}>
             <div class="info">
                 <${Logo}/>
-                This is a mostly-complete port of <a href="https://store.steampowered.com/app/504230/Celeste/">Celeste</a> to the browser using dotnet's <pre>wasmbrowser</pre> template and <a href="https://github.com/RedMike/FNA.WASM.Sample">FNA.WASM.Sample</a><br>
-
-                It needs around 1.6gb of memory and will probably not work on mobile devices<br><br>
-
+                <div class="inner">
+                <div>
+                This is a mostly-complete port of <a href="https://store.steampowered.com/app/504230/Celeste/">Celeste</a> to the browser using dotnet's <pre>wasmbrowser</pre> template and <a href="https://github.com/RedMike/FNA.WASM.Sample">FNA.WASM.Sample</a>.<br>
+                </div>
+                <div>It needs around 1.6GB of memory and will probably not work on mobile devices.<br><br></div>
                 ${DRM && "You will need to own Celeste to play this. Make sure you have it downloaded and installed on your computer." || ""}<br>
-                ${!window.SINGLEFILE && "This will download around ~700mb of assets to your browser's local storage." || ""}<br><br>
+                ${!window.SINGLEFILE && "This will download around ~700MB of assets to your browser's local storage." || ""}<br><br>
 
                 ${$if(use(this.downloading),
-        $if(use(this.showprogress), html`<p> progress: <progress value=${use(this.progress)} max="100"></progress></p>`),
-        html`<button on:click=${download}>Download Assets</button>`,
+        $if(use(this.showprogress), html`
+          <div>
+          <p>Downloading... (${use(this.progress)}% done)</p>
+          <div id="bar" style="width: 100%; height: 0.5em; background-color: var(--surface1); border-radius: 0.1em;">
+            <div id="progress" style=${{
+              width: use`${this.progress}%`,
+              height: "0.5em",
+              backgroundColor: "var(--accent)",
+              borderRadius: "0.1em"
+            }}></div>
+          </div>
+          </div>
+        `),
+        html`<button class="important" on:click=${download}><span class="material-symbols-rounded">download</span><span class="label">Download Assets</span></button>`,
 
     )}
     <br>
@@ -159,15 +196,16 @@ function IntroSplash() {
 
             ${$if(use(this.downloaded),
         $if(use(this.decrypted),
-            html`<button on:click=${finish}>Continue</button>`,
+            html`<button class="important" on:click=${finish}><span class="material-symbols-rounded">stadia_controller</span><span class="label">Play</span></button>`,
             html`
                 <div>
-                    <p>Downloaded assets. Now you need to decrypt them. Find the game files directory for celeste and upload Celeste.Content.dll</p>
-                    <button on:click=${decrypt}>Decrypt Assets</button>
+                    <p>Downloaded assets. Now you need to decrypt them. Find the game files directory for your copy of Celeste and upload Celeste.Content.dll.</p>
+                    <button class="important" on:click=${decrypt}><span class="material-symbols-rounded">encrypt</span><span class="label">Decrypt</span>s</button>
                     <br>
                     <p>${use(this.decrypterror)}</p>
                 </div>`
         ))}
+            </div>
             </div>
         </main>
     `
